9832f9fe4820a10a751d802356011b9cbd946243
Will Lytle <wlytle89@gmail.com>
2020-08-13 21:54:01 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
       }
     }
 
+  programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -56,6 +59,7 @@ def dennis_ritchies_language
       }
     }
 
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -82,6 +86,12 @@ def adding_matz
       }
     }
 
+  programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
+
+programmer_hash
 end
 
 def changing_alan
@@ -103,6 +113,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+  alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash
+
 
 end
 
@@ -125,5 +139,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  programmer_hash
 end
